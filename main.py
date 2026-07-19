6ce8ffc0ceada63650fb67c56f24d9febd8dc299
Omm13 <miriyalaomm@gmail.com>
2026-07-20 03:19:21 +0530

    New: Whole new Code

--- main.py
+++ main.py
@@ -1,74 +1,86 @@
-# Import necessary libraries
-import speech_recognition as sr
 import pyttsx3
 import datetime
 import wikipedia
 import webbrowser
-import os
 import pyjokes
 
-# Initialize the speech Engine
+
+# Initialize voice engine
 def speak(text):
     print(f"Assistant: {text}")
-    try:
-        engine = pyttsx3.init()
-        engine.say(text)
-        engine.runAndWait()
-    except Exception as e:
-        print(f"Error occurred: {e}")
 
-# Wish The User
+    engine = pyttsx3.init()
+
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', 150)
+
+    volume = engine.getProperty('volume')
+    engine.setProperty('volume', 1.0)
+
+    engine.say(text)
+    engine.runAndWait()
+    engine.stop()
+
 def wish_user():
-    hour = int(datetime.datetime.now().hour)
-    if 0 <= hour < 12:
+    hour = datetime.datetime.now().hour
+
+    if hour < 12:
         speak("Good Morning!")
-    elif 12 <= hour < 18:
+    elif hour < 18:
         speak("Good Afternoon!")
     else:
         speak("Good Evening!")
-    speak("I am your assistant. How can I help you today?")
 
-# Simulate User Voice command
+    speak("I am your voice assistant. How can I help you?")
+
+
 def take_command():
-    return input("You:  ").lower()
+    return input("You: ").lower()
 
-# Main Assistant Function
+
 def run_assistant():
+
     wish_user()
+
     while True:
+
         query = take_command()
 
-        if 'wikipedia' in query:
-            speak('Searching Wikipedia...')
+        if "wikipedia" in query:
+            speak("Searching Wikipedia")
+
             query = query.replace("wikipedia", "")
-            results = wikipedia.summary(query, sentences=2)
-            speak("According to Wikipedia")
-            speak(results)
 
-        elif 'open youtube' in query:
-            webbrowser.open("youtube.com")
+            try:
+                result = wikipedia.summary(query, sentences=2)
+                speak("According to Wikipedia")
+                speak(result)
 
-        elif 'open google' in query:
-            webbrowser.open("google.com")
+            except:
+                speak("Sorry, I could not find information.")
 
-        elif 'play music' in query:
-            music_dir = 'C:\\Users\\YourUsername\\Music'
-            songs = os.listdir(music_dir)
-            os.startfile(os.path.join(music_dir, songs[0]))
+        elif "open youtube" in query:
+            speak("Opening YouTube")
+            webbrowser.open("https://youtube.com")
 
-        elif 'the time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")
-            speak(f"The time is {strTime}")
+        elif "open google" in query:
+            speak("Opening Google")
+            webbrowser.open("https://google.com")
 
-        elif 'joke' in query:
+        elif "time" in query:
+            current_time = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"The time is {current_time}")
+
+        elif "joke" in query:
             joke = pyjokes.get_joke()
             speak(joke)
 
-        elif 'exit' in query or 'quit' in query:
-            speak("Goodbye!")
+        elif "exit" in query or "bye" in query:
+            speak("Goodbye")
             break
 
         else:
-            speak("I didn't understand that. Can you please repeat?")
-    
-    run_assistant()
+            speak("I did not understand. Please try again.")
+
+
+run_assistant()
